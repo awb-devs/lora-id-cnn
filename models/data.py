1251b5df9b1fbfa987f7e3c95d46fe813f69f46c
AWB <220265642+awb-devs@users.noreply.github.com>
2025-11-30 14:39:14 -0500

    dataset-gen improvements & experiment arg parsing

--- models/data.py
+++ models/data.py
@@ -7,6 +7,78 @@ from numpy import complex64 as c64
 from pathlib import Path
 from torch.utils.data import Dataset
 
+"""
+todo:
+    - docs
+    - move random sampling to python script
+    - windowing
+    - decimation
+"""
+
+def generate_labelsets(
+        dataset: str,
+        root='dataset',
+        devices=['esp32A', 'esp32B', 'esp32C', 'esp32D', 'esp32E', 'esp32F'],
+        trainN=600,
+        testN=400,
+        ):
+    """Create label sets by random sampling"""
+    train_labels: List[Tuple[int, Path]] = []
+    test_labels: List[Tuple[int, Path]] = []
+    for label, device in enumerate(devices):
+        device_labels = []
+        subpath = Path(f"{root}/{dataset}/{device}")
+        for i, file in enumerate(subpath.glob('*.cfile')):
+            device_labels.append((label, file))
+        if len(device_labels) < trainN+testN:
+            print(f"Warning: insufficient samples for {device}. Expected {testN + trainN}, got {len(device_labels)}. Test and or train sets may be underpopulated.")
+        nplabels = np.array(device_labels)
+        np.random.shuffle(nplabels)
+        device_tr_labels = nplabels[:trainN]
+        device_ts_labels = nplabels[trainN:trainN+testN]
+        train_labels.extend(device_tr_labels)
+        test_labels.extend(device_ts_labels)
+    return np.array(train_labels), np.array(test_labels)
+
+class IQDataset(Dataset):
+    def __init__(self,
+                 labels,
+                 classes=['txA', 'txB', 'txC', 'txD', 'txE', 'txF'],
+                 noise=0.0,
+                 window=None,
+                 decimation=1,
+                 transform=None,
+                 target_transform=None
+                 ):
+        self.labels = labels
+        self.classes = classes
+        self.transform = transform
+        self.target_transform = target_transform
+        self.noise = noise
+        self.window = window
+        self.decimation = decimation
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        # get raw samples
+        label, path = self.labels[idx]
+        buf = np.fromfile(path, dtype=c64)
+        buf = np.stack([buf.real, buf.imag], axis=0)
+        if self.noise != 0.0:
+            pass
+        if self.window is not None:
+            pass
+        if self.decimation != 1:
+            buf = buf[::self.decimation]
+        if self.transform:
+            buf = self.transform(buf)
+        if self.target_transform:
+            buf = self.target_transform(label)
+        return buf, label
+
+"""
 class IQDataset(Dataset):
     def __init__(self, 
                  dataset,
@@ -43,4 +115,4 @@ def labels_from_dir(path: str, devices: List[str], data_class: str, train: str) 
         for i, file in enumerate(subpath.glob('*.cfile')):
             labels.append((label, file))
     return labels
-
+"""
--- models/experiments.py
+++ models/experiments.py
@@ -1,16 +1,107 @@
+import numpy as np
 import torch
 import torchvision
+import sys
+import argparse
 
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
 import torch.optim as optim
 from pathlib import Path
 
+
 from train import train
 from test import test
-from data import IQDataset
+from data import generate_labelsets, IQDataset
 from report import generate_report
 import models
+
+
+experiments = {
+        'create_labelsets': False,
+        'wired_basic': False,
+        'room_basic': False
+}
+
+args = sys.argv[1:]
+for arg in args:
+    experiments[arg] = True
+    
+if experiments['create_labelsets']:
+    wired_tr, wired_ts = generate_labelsets('wired_250_24')
+    room_tr, room_ts = generate_labelsets('room_250_16')
+    np.savez('dataset/labels.npz', wired_tr=wired_tr, wired_ts=wired_ts, room_tr=room_tr, room_ts=room_ts)
+
+if experiments['wired_basic']:
+    name = 'wired_basic'
+    dataset = 'wired_250_24'
+    batch_size = 200
+    lr = 0.001
+    epochs = 50
+
+    stored_labels = np.load('dataset/labels.npz', allow_pickle=True)
+    tr_labels = stored_labels['wired_tr']
+    ts_labels = stored_labels['wired_ts']
+    
+    tr_data = IQDataset(tr_labels)
+    ts_data = IQDataset(ts_labels)
+
+    tr_loader = DataLoader(tr_data, batch_size, shuffle=True)
+    ts_loader = DataLoader(ts_data, batch_size, shuffle=True)
+    
+    network = models.Multi_Net()
+    optim = optim.Adam(network.parameters(), lr)
+
+    Path(name).mkdir(exist_ok=True)
+
+    loss = train(name, network, optim, tr_loader, epochs)
+    results = test(name, network, ts_loader)
+    generate_report(
+            name,
+            dataset,
+            lr,
+            epochs,
+            "",
+            loss,
+            results,
+            ts_data.classes
+            )
+
+if experiments['room_basic']:
+    name = 'room_basic'
+    dataset = 'room_250_16'
+    batch_size = 200
+    lr = 0.001
+    epochs = 50
+
+    stored_labels = np.load('dataset/labels.npz', allow_pickle=True)
+    tr_labels = stored_labels['room_tr']
+    ts_labels = stored_labels['room_ts']
+    
+    tr_data = IQDataset(tr_labels)
+    ts_data = IQDataset(ts_labels)
+
+    tr_loader = DataLoader(tr_data, batch_size, shuffle=True)
+    ts_loader = DataLoader(ts_data, batch_size, shuffle=True)
+
+    network = models.Multi_Net()
+    optim = optim.Adam(network.parameters(), lr)
+
+    Path(name).mkdir(exist_ok=True)
+
+    loss = train(name, network, optim, tr_loader, epochs)
+    results = test(name, network, ts_loader)
+    generate_report(
+            name,
+            dataset,
+            lr,
+            epochs,
+            "",
+            loss,
+            results,
+            ts_data.classes
+            )
+
 
 """
 Experiment Index
@@ -32,73 +123,4 @@ Experiment Index
 
 """
 
-"""
-Experiment 1
-Basic Model Trained on Wired Data
-"""
-"""
-name = "Exp01"
-dataset = 'wired_250_24'
-batch_size = 200
-lr = 0.001
-epochs = 50
-
-tr_data = IQDataset(dataset, 'train')
-tr_dataloader = DataLoader(tr_data, batch_size, shuffle=True)
-
-ts_data = IQDataset(dataset, 'test')
-ts_dataloader = DataLoader(ts_data, batch_size, shuffle=True)
-
-network = models.Multi_Net()
-optim = optim.Adam(network.parameters(), lr)
-
-Path(name).mkdir(exist_ok=True)
-
-loss = train(name, network, optim, tr_dataloader, epochs)
-results = test(name, network, ts_dataloader)
-generate_report(
-        name,
-        dataset,
-        lr,
-        epochs,
-        "",
-        loss,
-        results,
-        ts_data.classes
-        )
-"""
-"""
-Experiment 2
-Basic Model Trained on Room Data
-"""
-
-name = "Exp02-room"
-dataset = 'room_250_16'
-batch_size = 200
-lr = 0.001
-epochs = 50
-
-tr_data = IQDataset(dataset, 'train')
-tr_dataloader = DataLoader(tr_data, batch_size, shuffle=True)
-
-ts_data = IQDataset(dataset, 'test')
-ts_dataloader = DataLoader(ts_data, batch_size, shuffle=True)
-
-network = models.Multi_Net()
-optim = optim.Adam(network.parameters(), lr)
-
-Path(name).mkdir(exist_ok=True)
-
-loss = train(name, network, optim, tr_dataloader, epochs)
-results = test(name, network, ts_dataloader)
-generate_report(
-        name,
-        dataset,
-        lr,
-        epochs,
-        "",
-        loss,
-        results,
-        ts_data.classes
-        )
 
